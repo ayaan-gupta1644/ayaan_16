e13b162b6e5a616d71900c8cf8a67379b79fac69
Ayaan Gupta <ayaangupta79080@gmail.com>
2025-09-16 11:47:14 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,16 @@
+import random
+a=random.randrange(1,50)
+n=int(input("Print a number between 1 to 50: " ))
+if n==a:
+    print("You win")    
+else:
+    n2=int(input("Try again: "))
+    if n2==a:
+        print("You win")
+    else:
+        n3=int(input("Last chance: "))
+        if n3==a:
+            print("You win")
+        else:
+            print("You lose, the number was ",a)
+print("Thank you for playing")
